c9f42d35bd2d378384642364f97d3c025d5bf937
Seesko <seeskome@seeskosee.com>
2024-04-26 13:18:40 -0700

    API updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,20 @@
 """
 Creates and runs a Flask web application for a RESTful API.
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Return a JSON-formatted 404 response"""
+    return (jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
@@ -18,7 +25,6 @@ def close_storage(exception):
 
 
 if __name__ == "__main__":
-    from os import getenv
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(getenv("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
