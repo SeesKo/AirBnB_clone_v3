21981dc401a0cf34b34d531c0b25245c58c69852
Seesko <seeskome@seeskosee.com>
2024-04-26 05:29:19 -0700

    api views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+from api.v1.views.index import *
 from flask import Blueprint
 
 # Create the blueprint with the specified URL prefix
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
-# Wildcard import everything from `api.v1.views.index`
-# (PEP 8 may complain about this, but it's required for Flask blueprints)
-from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route("/status", methods=["GET"])
 def get_status():
     """Returns the status of the API"""
