02a377f68f05424746485fcaa2b0271507a291e3
Seesko <seeskome@seeskosee.com>
2024-04-26 07:55:09 -0700

    api updates

---
+++ api/v1/views/index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route("/status", methods=["GET"])
+def get_status():
+    """Returns the status of the API"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=["GET"])
+def get_stats():
+    """Returns the count of each object type"""
+    # Using the count() method to get counts for each object type
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(stats)
