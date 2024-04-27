a22d48cce949a0d840c99639854319c0853d0188
Seesko <seeskome@seeskosee.com>
2024-04-27 06:02:32 -0700

    create and run flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Creates and runs a Flask web application for a RESTful API.
 """
-from flask import Flask, jsonify, make_response, Blueprint
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
@@ -15,22 +15,19 @@ app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {'origins': ['0.0.0.0']}})
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Return a JSON-formatted 404 response"""
+    return jsonify({"error": "Not found"}), 404
+
+
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """ closes down current session """
+def close_storage(exception):
+    """Closes storage when app context is torn down"""
     storage.close()
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    """
-    a handler for 404 errors that returns a
-    JSON-formatted 404 status code response
-    """
-    return (jsonify({"error": "Not found"}), 404)
-
-
 if __name__ == "__main__":
-    hosts = getenv("HBNB_API_HOST", default='0.0.0.0')
-    ports = int(os.getenv("HBNB_API_PORT", default=5000))
-    app.run(host=hosts, port=ports, threaded=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", "5000"))
+    app.run(host=host, port=port, threaded=True)
