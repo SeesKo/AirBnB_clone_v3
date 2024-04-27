7f2cac0eaec78ea454ee5d3320abb9af5520b505
Seesko <seeskome@seeskosee.com>
2024-04-27 05:34:01 -0700

    API updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
 """
-Creates and runs a Flask web application for a RESTful API.
+creating blueprint for flask
 """
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, Blueprint,  make_response, jsonify
 from models import storage
+import os
+from flask_cors import CORS
 from api.v1.views import app_views
-from os import getenv
+
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """Return a JSON-formatted 404 response"""
-    return jsonify({"error": "Not found"}), 404
+cors = CORS(app, resources={r"/*": {'origins': ['0.0.0.0']}})
 
 
 @app.teardown_appcontext
-def close_storage(exception):
-    """Closes storage when app context is torn down"""
+def teardown_appcontext(self):
+    """ closes down current session """
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    a handler for 404 errors that returns a
+    JSON-formatted 404 status code response
+    """
+    return (jsonify({"error": "Not found"}), 404)
+
+
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(getenv("HBNB_API_PORT", "5000"))
-    app.run(host=host, port=port, threaded=True)
+    hosts = os.getenv("HBNB_API_HOST", default='0.0.0.0')
+    ports = int(os.getenv("HBNB_API_PORT", default=5000))
+    app.run(host=hosts, port=ports, threaded=True)
