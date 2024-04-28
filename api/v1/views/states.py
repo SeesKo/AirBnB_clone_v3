d1b76b656900575147569d7b4f0525909bd02269
Seesko <seeskome@seeskosee.com>
2024-04-27 17:03:38 -0700

    managing State objects with REST

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,7 +8,7 @@ from models import storage
 from models.state import State
 
 
-@app_views.route("/states", methods=["GET"])
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
 def get_states():
     """Returns a list of all State objects"""
     all_states = storage.all(State).values()
@@ -16,16 +16,17 @@ def get_states():
     return jsonify(states_list)
 
 
-@app_views.route("/states/<state_id>", methods=["GET"])
+@app_views.route("/states/<state_id>", strict_slashes=False)
 def get_state(state_id):
     """Returns a State object by ID"""
     state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    return jsonify(state.to_dict())
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        return abort(404)
 
 
-@app_views.route("/states/<state_id>", methods=["DELETE"])
+@app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object by ID"""
     state = storage.get(State, state_id)
@@ -33,24 +34,28 @@ def delete_state(state_id):
         abort(404)
     storage.delete(state)
     storage.save()
-    return jsonify({})
+    return jsonify({}), 200
 
 
-@app_views.route("/states", methods=["POST"])
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
 def create_state():
     """Creates a new State"""
-    if not request.json:
-        abort(400, "Not a JSON")
-    if "name" not in request.json:
+    if request.content_type != "application/json":
+        return abort(404, "Not a JSON")
+    if not request.get_json():
+        return abort(400, "Not a JSON")
+
+    kwargs = request.get_json()
+
+    if "name" not in kwargs:
         abort(400, "Missing name")
-    state_data = request.json
-    new_state = State(name=state_data["name"])
-    storage.new(new_state)
-    storage.save()
-    return jsonify(new_state.to_dict()), 201
+
+    new_state = State(**kwargs)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 200
 
 
-@app_views.route("/states/<state_id>", methods=["PUT"])
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
 def update_state(state_id):
     """Updates a State object by ID"""
     state = storage.get(State, state_id)
