371177314bc8503781b98146e93726b948831d76
Seesko <seeskome@seeskosee.com>
2024-04-28 19:42:27 -0700

    password definition resolution

--- models/user.py
+++ models/user.py
@@ -14,14 +14,14 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column('password', String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
 
@@ -29,16 +29,17 @@ class User(BaseModel, Base):
         """Initializes user with hashed password"""
         if "password" in kwargs:
             # Hash the password before storing it
-            kwargs["password"] = hashlib.md5(
-                    kwargs["password"].encode()).hexdigest()
+            kwargs["_password"] = hashlib.md5(
+                kwargs["password"].encode()).hexdigest()
+            del kwargs["password"]
         super().__init__(*args, **kwargs)
 
     @property
     def password(self):
-        return self.__password
+        return self._password
 
     @password.setter
     def password(self, new_password):
         """Hash the new password before storing"""
-        self.__password = hashlib.md5(
+        self._password = hashlib.md5(
                 new_password.encode()).hexdigest()
