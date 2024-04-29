ec6d5e4bdce22e6c85e1f01e105abff8295d21eb
Seesko <seeskome@seeskosee.com>
2024-04-28 20:22:35 -0700

    conversion to datetime objects

--- models/base_model.py
+++ models/base_model.py
@@ -28,24 +28,18 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            else:
-                self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-            else:
-                self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
-                self.id = str(uuid.uuid4())
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        datetime_format = "%Y-%m-%dT%H:%M:%S.%f"
+        for key, value in kwargs.items():
+            if key == '__class__':
+                continue
+            setattr(self, key, value)
+            if key == 'created_at' and isinstance(value, str):
+                self.created_at = datetime.strptime(value, datetime_format)
+            if key == 'updated_at' and isinstance(value, str):
+                self.updated_at = datetime.strptime(value, datetime_format)
 
     def __str__(self):
         """String representation of the BaseModel class"""
